4d5d845f37c05b9e20b69af023be6f2a82619a0e
Indexyz <jiduye@gmail.com>
2017-11-01 19:52:58 +0800

    work(forward): add enable trains

--- shadowsocks/relay/forwardrelay.py
+++ shadowsocks/relay/forwardrelay.py
@@ -1,9 +1,10 @@
-from shadowsocks.trains import traffic_able
+from shadowsocks.trains import traffic_able, enable_able
 import threading
 
-class ForwardRelay(threading.Thread, traffic_able.TrafficAble):
+class ForwardRelay(threading.Thread, traffic_able.TrafficAbl, enable_able.EnableAble):
     def __init__(self):
         threading.Thread.__init__(self)
+        enable_able.EnableAble.__init__(self)
 
     def run(self):
         pass
