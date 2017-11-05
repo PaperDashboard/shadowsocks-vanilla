de209a3ddfd632723aa91d3aa617d5d893657260
Indexyz <jiduye@gmail.com>
2017-11-05 17:18:29 +0800

    feat(api): support api addTraffic method

--- shadowsocks/api/sakura.py
+++ shadowsocks/api/sakura.py
@@ -8,6 +8,15 @@ def _get_with_key(url, key):
     res = urllib2.urlopen(req)
     return json.load(res)
 
+def _post_with_key(url, key, data):
+    opener = urllib2.build_opener(urllib2.HTTPHandler)
+    request = urllib2.Request(url, data=data)
+    request.add_header('Content-Type', 'application/json')
+    request.add_header('X-Key', key)
+    request.get_method = lambda: 'PUT'
+    res = opener.open(request)
+    return json.load(res)
+
 class SakuraAPI(object):
     def __init__(self, site, api_key):
         self._site = site
@@ -17,4 +26,11 @@ class SakuraAPI(object):
         return _get_with_key(
             self._site + "/users",
             self._api_key
+        )
+
+    def addTraffic(self, user, traffic): 
+        return _post_with_key(
+            self._site + "/user/" + user.id + "/addTraffic",
+            self._api_key,
+            json.dumps({"value": traffic})
         )
